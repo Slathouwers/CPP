85010a891f4581f5ef28e3fa55dd98f01e6b9803
Slathouwers <sander.lathouwers@live.be>
2022-02-05 20:22:30 +0000

    main implemented

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,32 +6,18 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 09:13:59 by slathouw          #+#    #+#             */
-/*   Updated: 2022/02/03 09:50:53 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/02/04 11:32:57 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Contact.hpp"
+# include "Phonebook.hpp"
 
 
 int	main(void)
 {
-	Contact contacts[2];
-	const std::string *fieldNames = contacts[0].get_fieldNames();
-
-	for (int i = 0; i < 2; i++)
-		contacts[i].init(i);
-
-	std::cout << "\033[2J";
-	for (int i = 0; i < 5; i++)
-		std::cout << " | " << fieldNames[i];
-	std::cout << "|" << std::endl;
+	Phonebook phonebook;
 	
-	for (int i = 0; i < 2; i++)
-	{
-		std::string *contents = contacts[i].get_fieldContents();
-		for (int i = 0; i < 5; i++)
-			std::cout << "|" << contents[i];
-		std::cout << "|" << std::endl;
-	}
+	phonebook = Phonebook();
+	phonebook.open();
 	return (0);
 }
