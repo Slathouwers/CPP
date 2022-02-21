6cdbff6be8443440c64b077283d0bdbfdc9e557e
slathouw <sander.lathouwers@live.be>
2022-02-21 11:59:49 +0100

    CPP ex02 tests added

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -3,27 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 09:22:23 by slathouw          #+#    #+#             */
-/*   Updated: 2022/02/10 09:07:29 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/02/21 11:56:39 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "FragTrap.hpp"
 #include "ScavTrap.hpp"
+
 
 int main(void)
 {
 	ScavTrap scav("MyScav");
 	ClapTrap clap("MyClap");
+	FragTrap frag("myFrag");
+	FragTrap cpyFrag(frag);
 	
-	ScavTrap deffy;
+	FragTrap deffy;
 	
 	deffy.setName("Deffy Default");
 
-	deffy.guardGate();
-	scav.attack(clap.getName());
-	scav.takeDamage(5000);
-	scav.attack(deffy.getName());
+	scav.guardGate();
+	frag.attack(scav.getName());
+	frag.takeDamage(5000);
+	frag.attack(deffy.getName());
 	
+	cpyFrag.highFivesGuys();
 }
